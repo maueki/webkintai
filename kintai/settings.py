3dfa5a1c766d849be3b080677da3e36e53083413
Masaru Ueki <masaru.ueki@itage.co.jp>
2016-08-24 22:48:14 +0900

    app -> work

--- kintai/settings.py
+++ kintai/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
+    'work.apps.WorkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
