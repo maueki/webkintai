88742c484d5aa45428b8eac1a0240c3dc2fa2908
Ueki Masaru <masaru.ueki@itage.co.jp>
2016-08-25 17:15:07 +0900

    ログイン処理追加

--- work/views.py
+++ work/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     return HttpResponse("Hello, world. You're at the work index.")
